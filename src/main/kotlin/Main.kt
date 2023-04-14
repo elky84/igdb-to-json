6e1fc0b98164840356701b04eb7599740056ce75
elky <elky@op.gg>
2023-04-14 23:43:59 +0900

    게임 목록 최대 10개까지만 쿼리 가능해서, 10개씩 잘라서 쿼리하도록 수정. 게임 목록 json 비교하고 원본에 값 입력하게 처리, 게임 목록 비교 파일명, 이미지 크기 규격을 설정 파일에서 관리

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,10 +4,48 @@ import com.api.igdb.exceptions.RequestException
 import com.api.igdb.request.*
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import dto.GameInfo
 import proto.*
 import java.io.File
 import java.util.*
+
+fun mergeJsonLists(jsonList1: String, jsonList2: String, prioritizeFirst: List<String>, idColumn: String): String {
+    val mapper = ObjectMapper().registerKotlinModule()
+    val list1 = mapper.readValue<List<LinkedHashMap<String, Any>>>(jsonList1)
+    val list2 = mapper.readValue<List<LinkedHashMap<String, Any>>>(jsonList2)
+
+    val mergedList = mutableListOf<Map<String, Any>>()
+
+    // Merge items by ID column
+    val map1 = list1.associateBy { it[idColumn] }
+    val map2 = list2.associateBy { it[idColumn] }
+    val ids = (map1.keys + map2.keys).distinct()
+    for (id in ids) {
+        val item1 = map1[id]
+        val item2 = map2[id]
+        val mergedItem = LinkedHashMap<String, Any>()
+        if(item1 != null) {
+            if(item1[idColumn] != null)
+            {
+                mergedItem[idColumn] = item1[idColumn] as Any
+            }
+
+            item1.keys.filter { it != idColumn && prioritizeFirst.contains(it) }.forEach { key ->
+                mergedItem[key] = item1[key]!!
+            }
+        }
+
+        item2?.keys?.filter { !prioritizeFirst.contains(it) }?.forEach { key ->
+            mergedItem[key] = item2[key]!!
+        }
+
+        mergedList.add(mergedItem)
+    }
+
+    return mapper.writeValueAsString(mergedList)
+}
 
 fun main(args: Array<String>) {
     val classLoader = Thread.currentThread().contextClassLoader
@@ -19,50 +57,61 @@ fun main(args: Array<String>) {
     val clientId = properties.getProperty("igdb.client-id")
     val clientSecret = properties.getProperty("igdb.client-secret")
     val iconBaseUrl = properties.getProperty("igdb.icon.base-url")
-    val gameIds = properties.getProperty("igdb.game.id")
+    val gameIds = properties.getProperty("igdb.game.id").split(",").toList()
     val getArtwork = properties.getProperty("igdb.artwork")?.toBoolean() ?: false
+    val imageSize = properties.getProperty("igdb.image.size")
+    val resultGameListFile = properties.getProperty("game-list.file.result")
+    val baseGameListFile = properties.getProperty("game-list.file.base")
 
     val token = TwitchAuthenticator.requestTwitchToken(clientId, clientSecret)
     IGDBWrapper.setCredentials(clientId, token!!.access_token)
 
     val gameInfos = mutableListOf<GameInfo>()
 
-    for(game in games("id = $gameIds")) {
-        println(game)
+    gameIds.chunked(10).forEach { chunk ->
+        for(game in games("id = (${chunk.joinToString("," )})")) {
+            println(game)
 
-        for(video in game.videosList){
-            val videos = video(video.id)
-            for(v in videos) {
-                println("$v")
-            }
-        }
-
-        if(getArtwork) {
-            for(artwork in game.artworksList){
-                val artworks = artwork(artwork.id)
-                for(artworkData in artworks) {
-                    println("https://images.igdb.com/igdb/image/upload/t_original/${artworkData.imageId}.jpg")
+            for(video in game.videosList){
+                val videos = video(video.id)
+                for(v in videos) {
+                    println("$v")
                 }
             }
+
+            if(getArtwork) {
+                for(artwork in game.artworksList){
+                    val artworks = artwork(artwork.id)
+                    for(artworkData in artworks) {
+                        println("https://images.igdb.com/igdb/image/upload/t_original/${artworkData.imageId}.jpg")
+                    }
+                }
+            }
+
+            var coverData: Cover? = null
+            val covers = cover(game.cover.id)
+            for(cover in covers) {
+                println("https://images.igdb.com/igdb/image/upload/${imageSize}/${cover.imageId}.jpg")
+                coverData = cover
+                break
+            }
+
+            val gameInfo = GameInfo(game.id, game.name,
+                "https://images.igdb.com/igdb/image/upload/${imageSize}/${coverData?.imageId}.jpg",
+                "${iconBaseUrl}/${coverData?.imageId}.png")
+
+            gameInfos.add(gameInfo)
         }
-
-        var coverData: Cover? = null
-        val covers = cover(game.cover.id)
-        for(cover in covers) {
-            println("https://images.igdb.com/igdb/image/upload/t_original/${cover.imageId}.jpg")
-            coverData = cover
-            break
-        }
-
-        val gameInfo = GameInfo(game.id, game.name,
-            "https://images.igdb.com/igdb/image/upload/t_original/${coverData?.imageId}.jpg",
-            "${iconBaseUrl}/${coverData?.imageId}.png")
-
-        gameInfos.add(gameInfo)
     }
 
     val objectMapper = ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT)
-    File("game_list.json").writeText(objectMapper.writeValueAsString(gameInfos))
+    val resultGameListJson = objectMapper.writeValueAsString(gameInfos)
+    File(resultGameListFile).writeText(resultGameListJson)
+
+    val baseGameListJson = File(baseGameListFile).readText()
+
+    val final = mergeJsonLists(baseGameListJson, resultGameListJson, listOf("name"), "gameId")
+    File(baseGameListFile).writeText(final)
 }
 
 
